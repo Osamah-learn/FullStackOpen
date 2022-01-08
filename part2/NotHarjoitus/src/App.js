3e27d393038d1b76f4143ea6f697514ad8ca012a
Osamah-learn <osamah.ammer2@gmaill.com>
2022-01-08 20:33:20 +0200

    Node Harjoitus

---
+++ part2/NotHarjoitus/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Note from './components/Note'
+import { useState } from 'react'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState("") 
+  const [showAll, setShowAll] = useState(true)
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+    console.log(notesToShow)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleNoteChange} value={newNote} />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App
