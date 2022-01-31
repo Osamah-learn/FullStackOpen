55a3d6393efd225f47ec19e98100258f871305d5
Osamah-learn <osamah.ammer2@gmaill.com>
2022-01-31 17:33:51 +0200

    Tasks from 3.1 untile 3.8 part3

---
+++ part3/3.5/index.js
@@ -0,0 +1,62 @@
+/* Varaibales and imports */
+const express = require("express");
+const morgan = require("morgan");
+const route = require("./route");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
+const { json } = require("express/lib/response");
+const { process_params } = require("express/lib/router");
+const fileLoggerStram = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+/* initilaize app.use */
+const app = express();
+app.use(
+  /* We decide here what to show inside morgan logger */
+  morgan(
+    ":user :id :method :url :status :response-time ms - :res[content-length] :body - :req[content-length]",
+    { stream: fileLoggerStram }
+  )
+);
+app.use(
+  /* We decide here what to show inside morgan logger */
+  morgan(
+    ":user :id :method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
+  )
+);
+app.use(assignId);
+app.use(express.json());
+app.use("/api/", route);
+
+/* Morgan Tokens */
+
+/* here we bulid Token to show body info */
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+/* we return the rig id inside Morgan Token  */
+morgan.token("id", function getId(req) {
+  return req.id;
+});
+morgan.token("user", function (req, res, param) {
+  return "UserToken = ";
+});
+/* Functions */
+
+/* Generate Uniqe id with uuid */
+function assignId(req, res, next) {
+  req.id = uuidv4();
+  next();
+}
+/* Http Rrquest */
+/* Response after /  */
+app.get("/", (req, res) => {
+  res.send("This BackEnd bulid by Osamah Amer");
+});
+
+/*  start the server with app listen function */
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
